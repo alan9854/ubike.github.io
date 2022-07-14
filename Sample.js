2dac29b74ad5ae6091dd04c7993136d6311a4ee6
alan9854 <33366365+alan9854@users.noreply.github.com>
2022-07-14 17:00:26 +0800

    axios_await

--- Sample.js
+++ Sample.js
@@ -12,28 +12,15 @@ map.locate({setView: true, maxZoom: 18});
 const markerLayer = L.markerClusterGroup();
 
 
-function init() {
-	
-	//GetAuthorizationHeader();
-	
-  //GetApiResponse_location();
-}
-init();
-
-
-
-
-function GetAuthorizationHeader() {    
-  let tokenCode = '';
-  const AAA="grant_type:client_credentials";
+async function GetAuthorizationHeader() {    
+  let tokenCode = '';  
   const parameter = {
     grant_type:"client_credentials",
     client_id: "f4981645-e27e7c4f-7fcd-4daa",
     client_secret: "7f013ea8-24a6-40d5-aff9-b003979178e1"
   };
-
-  let auth_url = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token';
-  axios({	  
+  let auth_url = 'https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token';  
+  let res = await axios({	  
     method: 'POST',
     url: auth_url,
 	headers: {
@@ -42,85 +29,38 @@ function GetAuthorizationHeader() {
 	data:qs.stringify(parameter),
     dataType:'JSON',
   })
-  .then((res) => {
-    tokenCode = res.data.access_token;
-    //console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
-	//console.log('data', parameter);
-  })          
-}
-
-function GetApiResponse(){
-  const url = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/Taoyuan?%24top=50&%24format=JSON';
-  
-  
-  
-  
-  axios.all
-  axios.get(url, {
-    headers: GetAuthorizationHeader(),
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
-
-
-function GetApiResponse(){
-  const url = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/Taoyuan?%24top=50&%24format=JSON';
-  
-  axios.get(url, {
-    headers: GetAuthorizationHeader(),
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log(err);
-  })
-}
-
-function GetApiResponse_location(current_city){
+	tokenCode = res.data.access_token; 
+	return {Authorization: `Bearer ${tokenCode}`,}
 	
-  const url_1 = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Station/City/'+current_city+'?%24top=50&%24format=JSON';
-  const url_2 = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/City/'+current_city+'?%24top=50&%24format=JSON'; 
-  
-  
-  axios.all([
-		axios.get(url_1, {
-		headers: GetAuthorizationHeader()
-		}
-  ),
-		axios.get(url_2,{
-			headers: GetAuthorizationHeader()
-		}
-  )
- ])
-  .then(axios.spread((A,B) => {
+}
+
+async function GetApiResponse_location(current_city){
 	
-	//A.data.forEach(element=>console.log(element))   
-	markerLayer.clearLayers();
-	for (let i=0; i<A.data.length; i++) {
-		let pos_lat = A.data[i].StationPosition.PositionLat;
-		let pos_lon = A.data[i].StationPosition.PositionLon;
-		markerLayer.addLayer(L.marker(new L.LatLng(pos_lat, pos_lon)).bindPopup(A.data[i].StationName.Zh_tw));
-		//markerLayer.addLayer([pos_lat, pos_lon]);
-		//marker.bindPopup(A.data[i].StationName.Zh_tw);
-	}
+		const url_1 = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Station/City/'+current_city+'?%24top=50&%24format=JSON';
+		const url_2 = 'https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/City/'+current_city+'?%24top=50&%24format=JSON'; 	
+		axios.all([
+			axios.get(url_1, {					
+			headers: await GetAuthorizationHeader(),			
+			}
+			),
+			axios.get(url_2,{					
+			headers: await GetAuthorizationHeader(),
+			}
+			)
+		])		
+		.then(axios.spread((A,B) => {	   
+		markerLayer.clearLayers();
+		for (let i=0; i<A.data.length; i++) {
+			let pos_lat = A.data[i].StationPosition.PositionLat;
+			let pos_lon = A.data[i].StationPosition.PositionLon;
+			markerLayer.addLayer(L.marker(new L.LatLng(pos_lat, pos_lon)).bindPopup(A.data[i].StationName.Zh_tw));
+			}
 		map.addLayer(markerLayer);
-		map.setView(new L.LatLng(A.data[0].StationPosition.PositionLat, A.data[0].StationPosition.PositionLon), 11);
-	
-	
+		map.setView(new L.LatLng(A.data[0].StationPosition.PositionLat, A.data[0].StationPosition.PositionLon), 11);	
   }))
-  .catch((err) => {
-    console.log(err);
-  })
+		.catch((err) => { console.error(err) })	
 }
+
 
   city.addEventListener("change",(e)=>{
 	GetApiResponse_location(e.target.value);
@@ -141,65 +81,5 @@ function onLocationFound(e) {
 map.on('locationfound', onLocationFound);
 
 
-/*
-$(function () {
-    //GetAuthorizationHeader();
-    
-    GetApiResponse();    
-});
 
-function GetAuthorizationHeader() {    
-    const parameter = {
-        grant_type:"client_credentials",
-        client_id: "f4981645-e27e7c4f-7fcd-4daa",
-        client_secret: "7f013ea8-24a6-40d5-aff9-b003979178e1"
-    };
-    
-    let auth_url = "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token";
-        
-    $.ajax({
-        type: "POST",
-        url: auth_url,
-        crossDomain:true,
-        dataType:'JSON',                
-        data: parameter,
-        async: false,       			
-        success: function(data){            
-            $("#accesstoken").text(JSON.stringify(data));     
-			//console.log('url', url);
-			console.log('data', data);
-			//console.log('crossDomain', crossDomain);
-			
-			
-        },
-        error: function (xhr, textStatus, thrownError) {
-            
-        }
-    });          
-}
 
-function GetApiResponse(){    
-    let accesstokenStr = $("#accesstoken").text();    
-
-    let accesstoken = JSON.parse(accesstokenStr);    
-
-    if(accesstoken !=undefined){
-        $.ajax({
-            type: 'GET',
-            url:'https://tdx.transportdata.tw/api/basic/v2/Bike/Availability/Taoyuan?%24top=30&%24format=JSON',
-			//url: 'https://tdx.transportdata.tw/api/basic/v2/Rail/TRA/LiveTrainDelay?$top=30&$format=JSON',             
-            headers: {
-                "authorization": "Bearer " + accesstoken.access_token,                
-              },            
-            async: false,
-            success: function (Data) {
-                //$('#apireponse').text(JSON.stringify(Data));                
-                console.log('Data', Data);
-            },
-            error: function (xhr, textStatus, thrownError) {
-                console.log('errorStatus:',textStatus);
-                console.log('Error:',thrownError);
-            }
-        });
-    }
-}*/
